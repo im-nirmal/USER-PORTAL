3f73c4ba70ef568d456ae6880bdc083369cbcce4
im-nirmal <arnirmal19@gmail.com>
2024-05-22 15:47:38 +0530

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EditAdminComponent } from './edit-admin/edit-admin.component';
+import { CalendaerComponent } from './calendaer/calendaer.component';
+import { ChartComponent } from './chart/chart.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent,
+    EditAdminComponent,
+    CalendaerComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    HighchartsChartModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calendaer/calendaer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-calendaer',
+  templateUrl: './calendaer.component.html',
+  styleUrls: ['./calendaer.component.css'],
+  
+})
+export class CalendaerComponent {
+  selected = new Date()
+}
